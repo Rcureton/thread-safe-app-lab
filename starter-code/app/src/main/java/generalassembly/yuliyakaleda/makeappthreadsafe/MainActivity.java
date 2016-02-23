4a064c685069e97dc999b5e013ecdb289b39e0e7
Rcureton <rashad.cureton@gmail.com>
2016-02-23 17:01:20 -0500

    completed lab

--- starter-code/app/src/main/java/generalassembly/yuliyakaleda/makeappthreadsafe/MainActivity.java
+++ starter-code/app/src/main/java/generalassembly/yuliyakaleda/makeappthreadsafe/MainActivity.java
@@ -1,25 +1,31 @@
 package generalassembly.yuliyakaleda.makeappthreadsafe;
 
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.net.URI;
 
 public class MainActivity extends AppCompatActivity{
   private static final String TAG = "makeappthreadsafe";
   private static final int PICK_IMAGE_REQUEST = 1;
   private ImageView image;
   private Button change;
+  private ProgressBar mSpinner;
+  private Uri mSelectedImage;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -27,12 +33,14 @@ public class MainActivity extends AppCompatActivity{
     setContentView(R.layout.activity_main);
 
     change = (Button) findViewById(R.id.choose_button);
+    mSpinner=(ProgressBar)findViewById(R.id.progressBar1);
     image = (ImageView) findViewById(R.id.image);
     setProfileImage();
     change.setOnClickListener(new View.OnClickListener() {
       @Override
       public void onClick(View v) {
         changeProfileImage();
+
       }
     });
   }
@@ -42,24 +50,12 @@ public class MainActivity extends AppCompatActivity{
   public void onActivityResult(int requestCode, int resultCode, Intent data) {
     super.onActivityResult(requestCode, resultCode, data);
     if (requestCode == PICK_IMAGE_REQUEST && resultCode == MainActivity.RESULT_OK && null != data) {
-      Uri selectedImage = data.getData();
-      image.setImageURI(selectedImage);
+      mSelectedImage = data.getData();
+      image.setImageURI(mSelectedImage);
+      ThreadSafeClass thread= new ThreadSafeClass();
+      thread.execute();
 
-      //saves a new picture to a file
-      Bitmap bitmap = null;
-      try {
-        bitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(selectedImage));
-      } catch (FileNotFoundException e) {
-        Log.d(TAG, "Image uri is not received or recognized");
-      }
-      try {
-        PictureUtil.saveToCacheFile(bitmap);
-      } catch (IOException e) {
-        e.printStackTrace();
-      }
 
-      //provides a feedback that the image is set as a profile picture
-      Toast.makeText(this, "The image is set as a profile picture", Toast.LENGTH_LONG).show();
     }
   }
 
@@ -80,5 +76,43 @@ public class MainActivity extends AppCompatActivity{
     intent.setType("image/*");
     intent.setAction(Intent.ACTION_GET_CONTENT);
     startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST);
+  }
+
+  private class ThreadSafeClass extends AsyncTask<Void,Void,Void>{
+    Bitmap bitmap = null;
+
+
+    @Override
+    protected void onPreExecute() {
+
+      mSpinner.setVisibility(View.VISIBLE);
+    }
+    //TODO: the type that you are putting in to pass in the background
+    @Override
+    protected Void doInBackground(Void... params) {
+
+      //saves a new picture to a file
+
+      try {
+        bitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(mSelectedImage));
+      } catch (FileNotFoundException e) {
+        Log.d(TAG, "Image uri is not received or recognized");
+      }
+      try {
+        PictureUtil.saveToCacheFile(bitmap);
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+      return null;
+    }
+
+
+
+    @Override
+    protected void onPostExecute(Void aVoid) {
+      Toast.makeText(MainActivity.this,"The image was saved and it set as the profile picture",Toast.LENGTH_SHORT).show();
+      mSpinner.setVisibility(View.GONE);
+//      super.onPostExecute(aVoid);
+    }
   }
 }
